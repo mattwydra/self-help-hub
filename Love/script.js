860a9cd5d4046cb333ec54f4344a0707ac5347be
mattwydra <matthewwydra@gmail.com>
2025-03-05 07:27:59 -0500

    Add endpoint for Love

---
+++ Love/script.js
@@ -0,0 +1,18 @@
+// Select the generate button
+const generateButton = document.getElementById("hopecore-link");
+
+// Paths to image folders
+const regularImagePath = "assets/hc";
+const specialImagePath = "assets/daily-reminder-that-you-will-have-this";
+
+const resultContainer = document.getElementById("result");
+
+const imageElement = document.createElement("img");
+imageElement.src =
+  "./assets/daily-reminder-that-you-will-have-this/IMG_1047.png";
+imageElement.alt = "Generated Hopecore Image";
+imageElement.style.maxWidth = "80%";
+
+resultContainer.innerHTML = "";
+resultContainer.appendChild(imageElement);
+
