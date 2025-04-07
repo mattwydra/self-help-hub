9d851578b9d2f7cafe9d11d9554ef1728214c3a3
mattwydra <matthewwydra@gmail.com>
2025-04-07 17:01:46 -0400

    Add comment for bug fix request

--- endpoints/love/script.js
+++ endpoints/love/script.js
@@ -9,7 +9,7 @@ const resultContainer = document.getElementById("result");
 
 const imageElement = document.createElement("img");
 imageElement.src =
-  "./assets/daily-reminder-that-you-will-have-this/IMG_1047.png";
+  "./assets/daily-reminder-that-you-will-have-this/IMG_1047.png"; // Need github API
 imageElement.alt = "Generated Hopecore Image";
 imageElement.style.maxWidth = "80%";
 
